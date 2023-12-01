e468bcb9858228ea16b33fa830fac256881ab16c
Lizbeth <liz.p.salinas23123@gmial.com>
2023-12-01 15:41:12 -0600

    Acceso a recursos del celular (Voz, Compartir y Descargar Recursos)

--- MiPrimeraAplicacionProgressiva/obj/Debug/net8.0/MiPrimeraAplicacionProgressiva.AssemblyInfo.cs
+++ MiPrimeraAplicacionProgressiva/obj/Debug/net8.0/MiPrimeraAplicacionProgressiva.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiPrimeraAplicacionProgressiva")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a818375afecaac6e3f94dc3ee64a9cdd56f7e052")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiPrimeraAplicacionProgressiva")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiPrimeraAplicacionProgressiva")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
